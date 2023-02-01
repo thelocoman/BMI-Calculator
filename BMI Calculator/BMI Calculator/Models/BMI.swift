1ab3aff441b57b23a32878f890c458a0aa86fcf7
Tibor Lovasz <104306217+thelocoman@users.noreply.github.com>
2023-02-01 13:11:33 +0100

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Tibor Lovasz on 1/31/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
